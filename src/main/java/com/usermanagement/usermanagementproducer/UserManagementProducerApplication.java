788d0853dbafb8f26c606975c84618365bcfad6b
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-01-24 19:33:23 +0100

    project initialization

---
+++ src/main/java/com/usermanagement/usermanagementproducer/UserManagementProducerApplication.java
@@ -0,0 +1,13 @@
+package com.usermanagement.usermanagementproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usermanagement/usermanagementproducer/UserManagementProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanagement.usermanagementproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
